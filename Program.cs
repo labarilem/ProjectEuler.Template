2d4f6481f994bc1853644ef247a9405b47068bec
Marco Labarile <labarilem@gmail.com>
2017-06-13 23:43:38 +0200

    Added project

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ProblemProjectEuler.Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            stopWatch.Stop();
+            Console.WriteLine("Result: ");
+            Console.WriteLine("Elapsed time: " + stopWatch.Elapsed.ToString());
+        }
+    }
+}
