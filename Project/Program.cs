db9a3488abfafc81dc847f4b663d6455d92e7067
Marco Labarile <labarilem@gmail.com>
2017-08-07 19:51:17 +0200

    fixed indentation

--- Project/Program.cs
+++ Project/Program.cs
@@ -5,16 +5,16 @@ using System.Diagnostics;
 
 namespace ProblemProjectEuler.Template
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Stopwatch stopWatch = new Stopwatch();
-            stopWatch.Start();
+      Stopwatch stopWatch = new Stopwatch();
+      stopWatch.Start();
 
-            stopWatch.Stop();
-            Console.WriteLine("Result: ");
-            Console.WriteLine("Elapsed time: " + stopWatch.Elapsed.ToString());
-        }
+      stopWatch.Stop();
+      Console.WriteLine("Result: ");
+      Console.WriteLine("Elapsed time: " + stopWatch.Elapsed.ToString());
     }
+  }
 }
